20adb9e300d664051f9cc23aa2f46dccfe514756
Documenter.jl <documenter@juliadocs.github.io>
2021-08-26 13:49:08 +0000

    build based on dd28dd7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IndirectCoStateInit","category":"page"},{"location":"#IndirectCoStateInit","page":"Home","title":"IndirectCoStateInit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IndirectCoStateInit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IndirectCoStateInit]","category":"page"}]
+}
